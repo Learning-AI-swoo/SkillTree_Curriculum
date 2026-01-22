b57e9ede06695a419a3818528db7b4d9dfe1bed2
Learning-AI-swoo <learning.ai.swoo@gmail.com>
2026-01-22 09:42:16 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Replace 'Skilltree-Curriculum' with your actual GitHub repository name
+  // Replace 'Skilltree_Curriculum' with your actual GitHub repository name
   base: '/SkillTree-Curriculum/', 
   plugins: [react()],
   resolve: {
